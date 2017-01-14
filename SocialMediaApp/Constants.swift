3e669355495fb4a2405ff68bcd7ed21ab9549ac9
KHILL101 <KHILL101@users.noreply.github.com>
2017-01-13 21:47:41 -0500

    Sign in ui custom classes complete

---
+++ SocialMediaApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SocialMediaApp
+//
+//  Created by Kristian Hill on 1/13/17.
+//  Copyright © 2017 KristianH. All rights reserved.
+//
+
+import UIKit
+
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ SocialMediaApp/SigninField.swift
@@ -0,0 +1,29 @@
+//
+//  SigninField.swift
+//  SocialMediaApp
+//
+//  Created by Kristian Hill on 1/13/17.
+//  Copyright © 2017 KristianH. All rights reserved.
+//
+
+import UIKit
+
+class SigninField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 3.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
