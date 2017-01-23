01990f703cda1e727aad4c2dcbdf716108a2e8d1
KHILL101 <KHILL101@users.noreply.github.com>
2017-01-22 20:05:47 -0500

    Completed creating Firebase DB user

---
+++ SocialMediaApp/DataService.swift
@@ -0,0 +1,39 @@
+//
+//  DataService.swift
+//  SocialMediaApp
+//
+//  Created by Kristian Hill on 1/22/17.
+//  Copyright © 2017 KristianH. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+let DB_BASE = FIRDatabase.database().reference()
+
+class DataService {
+    
+    static let ds = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_POSTS = DB_BASE.child("posts")
+    private var _REF_USERS = DB_BASE.child("users")
+    
+    var REF_BASE: FIRDatabaseReference {
+        return _REF_BASE
+    }
+    
+    var REF_POSTS: FIRDatabaseReference {
+        return _REF_POSTS
+    }
+    
+    var REF_USERS: FIRDatabaseReference {
+        return _REF_USERS
+    }
+    
+    func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
+        REF_USERS.child(uid).updateChildValues(userData)
+    }
+    
+    
+}
